932638b41313e359e198d19a30a2676cf5105abf
DaniiGomezz <gomezjesushd@gmail.com>
2023-08-28 14:13:24 -0300

    feat: Se creo la carpeta controllers y dentro de ella el controlador para el usuario

---
+++ src/controllers/usuario.CRUD.js
@@ -0,0 +1,50 @@
+const Usuarios = require('../models/usuario.models');
+const ctrlUsuarios = {};
+
+// Obtener todos los usuarios
+ctrlUsuarios.obtenerlosUsuarios = async (req, res) => {
+    try {
+        const Users = await Usuarios.findAll({
+            where: {
+                estado: true
+            }
+        });
+
+        return res.json(Users);
+    } catch (error) {
+        console.log('Error al obtener los usuarios', error);
+        return res.status(500).json({
+            message: 'Error al obtener a los usuarios'
+        })
+    }
+}
+
+// Crear un nuevo usuario
+ctrlUsuarios.crearUsuario = async (req, res) => {
+    const {
+        nombre,
+        apellido,
+        email,
+        usuario,
+        contraseña,
+    } = req.body;
+
+    try {
+        const nuevoUsuario = new Usuarios({
+            nombre,
+            apellido,
+            email,
+            usuario,
+            contraseña
+        });
+
+        await nuevoUsuario.save();
+
+        return res.status(201).json({ message: '!!EL usuario fue creado exitosamente!!'})
+    } catch (error) {
+        console.log('Error al crear al usuario', error);
+        return res.status(500).json({ message: 'Hubo un error inesperado al crear al usuario'})
+    }
+}
+
+module.exports = ctrlUsuarios;
