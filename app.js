4c5b0d64461a8f0eb77fa91a4f6139a78ee79091
DaniiGomezz <gomezjesushd@gmail.com>
2023-08-28 12:30:27 -0300

    se configuro la conexion a la base de datos, las variables de entorno, se requirio y se configuro la conexion a la base de datos en el archivo de aranque  y se configuro middlewares en el archivo de arranque

--- app.js
+++ app.js
@@ -4,4 +4,17 @@ const morgan = require('morgan');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
+
+const { conexionBaseDatos } = require('./src/db/dbconnect');
+conexionBaseDatos();
+
 const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Servidor funcionando en el puerto ${port}`))
