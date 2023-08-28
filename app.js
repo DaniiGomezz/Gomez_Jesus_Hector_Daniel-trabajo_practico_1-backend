3620c54a2ed56cc552f1c471ce3fce96e4a137ae
DaniiGomezz <gomezjesushd@gmail.com>
2023-08-28 12:20:40 -0300

    Se instalaron las dependecias que necesita el servidor para que funcione y se configuro el archivo de arranque para arrancar el servidor

---
+++ app.js
@@ -0,0 +1,7 @@
+// Importamos las dependencias 
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+const app = express();
